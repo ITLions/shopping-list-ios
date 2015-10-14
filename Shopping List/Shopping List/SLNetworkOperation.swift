b3e8d61f621a0fe5d8dcc268b796d9141f2f0cd2
Pavel Gatilov <Pavel.Gatilov@thetrainline.com>
2015-10-14 15:08:36 +0100

    Network Operation

---
+++ Shopping List/Shopping List/SLNetworkOperation.swift
@@ -0,0 +1,13 @@
+//
+//  SLNetworkOperation.swift
+//  Shopping List
+//
+//  Created by Pavel Gatilov on 14/10/2015.
+//  Copyright © 2015 Pavel Gatilov. All rights reserved.
+//
+
+import UIKit
+
+class SLNetworkOperation: NSOperation {
+    
+}
